f961bb708398698dcb0352666cb1b2069145394e
jx-Shihehua <2305654418@qq.com>
2019-12-03 19:41:06 +0800

    教务管理的vo类

---
+++ src/com/ht/vo/CourseTypeVo.java
@@ -0,0 +1,54 @@
+package com.ht.vo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Created by 华 on 2019/12/3.
+ */
+@Entity
+public class CourseTypeVo {
+    /**
+     * 课程类别表
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Integer courseTypeId; //主键，标识列，自动生成
+    public String courseTypeName; //类别名称(公共课，专业课)
+    public String remark; //说明
+
+    @Override
+    public String toString() {
+        return "CourseType{" +
+                "courseTypeId=" + courseTypeId +
+                ", courseTypeName='" + courseTypeName + '\'' +
+                ", remark='" + remark + '\'' +
+                '}';
+    }
+
+    public Integer getCourseTypeId() {
+        return courseTypeId;
+    }
+
+    public void setCourseTypeId(Integer courseTypeId) {
+        this.courseTypeId = courseTypeId;
+    }
+
+    public String getCourseTypeName() {
+        return courseTypeName;
+    }
+
+    public void setCourseTypeName(String courseTypeName) {
+        this.courseTypeName = courseTypeName;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+}
