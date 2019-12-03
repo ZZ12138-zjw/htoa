f280bf55bdf0cd0ee3350f56de1d638fc3d07c78
XiaoEn-lwc <2681737638@qq.com>
2019-12-03 15:21:55 +0800

    宏图软件OA

---
+++ src/com/ht/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.ht.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by shkstart on 2019/12/3
+ * @author LaIWeiChun
+ */
+@Controller
+public class TestController {
+
+    @RequestMapping("/test")
+    public String test(){
+        System.out.println("我进来了");
+        return "";
+    }
+
+
+}
