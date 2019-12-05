377be17254c73ad7b6a90c4a38e88b969255d365
XiaoEn-lwc <2681737638@qq.com>
2019-12-05 11:23:58 +0800

    12/5 10:55 xiaoen

--- src/com/ht/controller/wj/DocumentController.java
+++ src/com/ht/controller/wj/DocumentController.java
@@ -1,9 +1,9 @@
 package com.ht.controller.wj;
 
+import com.ht.service.wj.DocumentService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import javax.annotation.Resource;
 import java.util.List;
 
