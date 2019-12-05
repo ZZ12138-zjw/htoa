d4456050f7ff7597a7d49934721562a68e9d67e4
XiaoEn-lwc <2681737638@qq.com>
2019-12-05 10:23:59 +0800

    12/4 22:22

--- src/com/ht/controller/xiaoen/DeptController.java
+++ src/com/ht/controller/xiaoen/DeptController.java
@@ -1,5 +1,7 @@
 package com.ht.controller.xiaoen;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.ht.service.xiaoen.IDeptService;
 import com.ht.vo.employee.DeptVo;
@@ -25,22 +27,26 @@ public class DeptController{
     @Autowired
     private IDeptService dept;
 
-    @RequestMapping("/dept_list")
-    public String  toDeptList(Map map, String currPage, HttpServletResponse response) throws IOException {
-        if (currPage==null || "".equals(currPage)){
-            currPage="1";
-        }
-        List deptList = dept.selectPage(Integer.parseInt(currPage),10);
-        map.put("deptCount",dept.selectCount());
-        map.put("deptList",deptList);
-        map.put("currPage",currPage);
-        return "dept_list";
+
+    @RequestMapping("/depList")
+    @ResponseBody
+    public Map depList(String page,  String limit){
+        Map map=new HashMap();
+        map.put("code",0);
+        map.put("msg"," ");
+        map.put("count",dept.selectCount());
+        JSONArray jsonArray=(JSONArray)JSON.toJSON(dept.selectPage(Integer.parseInt(page),Integer.parseInt(limit)));
+        map.put("data",jsonArray);
+        System.out.println(map.toString());
+        return map;
     }
+
 
     @RequestMapping("/to_dept_add")
     public String toDpetAdd(){
         return "dept_add";
     }
+
 
     @RequestMapping("/add")
     @ResponseBody
@@ -48,6 +54,23 @@ public class DeptController{
         dept.save(deptVo);
         return "success";
     }
+
+    @RequestMapping("/delete")
+    @ResponseBody
+    public String delete(DeptVo deptVo){
+        dept.delete(deptVo);
+        return "success";
+    }
+
+    @RequestMapping("/deletes")
+    @ResponseBody
+    public String deletes(String[] depIds){
+        System.out.println(depIds.toString());
+        return "success";
+    }
+
+
+
 
 
 }
