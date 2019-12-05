0de88892b7a2735c34498430feac3e5a86d82548
ZZ12138-zjw <2550053654@qq.com>
2019-12-05 11:11:27 +0800

    报修申请控制器

--- src/com/ht/controller/zz12138/RepairApplyController.java
+++ src/com/ht/controller/zz12138/RepairApplyController.java
@@ -1,8 +1,13 @@
 package com.ht.controller.zz12138;
 
+import com.ht.service.zz12138.IRepairManageService;
+import com.ht.vo.LogisticsCheck.RepairManageVo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+import java.util.Date;
 
 /**
  * @author @ZZ12138-zjw
@@ -12,11 +17,21 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/contro")
 public class RepairApplyController {
 
+    @Resource
+    IRepairManageService service;
+
     @ResponseBody
     @RequestMapping("/repairmanage")
-    public String repairmanage(String repairMan,String repairSort,String repairAddress,String startDate,String repairIndex){
-        System.out.println("提交维修申请");
-        System.out.println(repairMan+repairAddress+repairIndex+repairSort+startDate);
+    public String repairmanage(String repairMan,String repairSort,String repairAddress,String repairName,String repairDept){
+        RepairManageVo vo = new RepairManageVo();
+        vo.setRepairName(repairName);
+        vo.setRepairMan(repairMan);
+        vo.setRepairDept(repairDept);
+        vo.setRepairSort(repairSort);
+        vo.setRepairAddress(repairAddress);
+        vo.setRepairStatus("待维修");
+        vo.setStartDate(new Date());
+        service.addRepairMange(vo);
         return "success";
     }
 
