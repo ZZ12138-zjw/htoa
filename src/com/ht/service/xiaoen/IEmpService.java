94a3ea5649830c2aac7588a41c54feebb0995e9b
XiaoEn-lwc <2681737638@qq.com>
2019-12-06 09:01:24 +0800

    12/6 9:01 xiaoen

---
+++ src/com/ht/service/xiaoen/IEmpService.java
@@ -0,0 +1,29 @@
+package com.ht.service.xiaoen;
+
+import com.ht.vo.employee.DeptVo;
+import com.ht.vo.employee.EmpVo;
+
+import java.util.List;
+
+/**
+ * Created by shkstart on 2019/12/5
+ * @author LaIWeiChun
+ */
+public interface IEmpService  {
+
+
+    List<EmpVo> selectAll();
+
+    void delete(EmpVo empVo);
+
+    void update(EmpVo empVo);
+
+    EmpVo select(EmpVo empVo);
+
+    List<EmpVo> selectPage(int currPage,int pageSize);
+
+    int selectCount();
+
+    void save(EmpVo empVo);
+
+}
