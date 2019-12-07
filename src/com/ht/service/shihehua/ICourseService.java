184459ad9dadfc8499af88d5c5b1ead91dafe8e1
jx-Shihehua <2305654418@qq.com>
2019-12-07 08:35:57 +0800

    史何华 12/7 8:35

--- src/com/ht/service/shihehua/ICourseService.java
+++ src/com/ht/service/shihehua/ICourseService.java
@@ -2,9 +2,24 @@ package com.ht.service.shihehua;
 
 import com.ht.vo.educational.CourseVo;
 
+import java.util.List;
+
 /**
  * Created by 华 on 2019/12/4.
  */
 public interface ICourseService {
+
+    public List selCourse();
+
     public void addCourse(CourseVo courseVo);
+
+    public List selCoursePage(Integer currPage,Integer pageSize);
+
+    public CourseVo selCourse(Integer courseid);
+
+    public int selCourseCount();
+
+    public void updateCourse(CourseVo courseVo);
+
+    public void delCourse(CourseVo courseVo);
 }
--- src/com/ht/service/shihehua/impl/CourseServiceImpl.java
+++ src/com/ht/service/shihehua/impl/CourseServiceImpl.java
@@ -5,13 +5,45 @@ import com.ht.service.shihehua.ICourseService;
 import com.ht.vo.educational.CourseVo;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 /**
  * Created by 华 on 2019/12/4.
  */
 @Service
 public class CourseServiceImpl extends BaseDao implements ICourseService{
     @Override
+    public List selCourse() {
+        return listByHql("from CourseVo");
+    }
+
+    @Override
     public void addCourse(CourseVo courseVo) {
         addObject(courseVo);
+    }
+
+    @Override
+    public List selCoursePage(Integer currPage, Integer pageSize) {
+        return pageBySQL("select cs.*,ct.courseTypeName from course cs left join coursetype ct on cs.courseTypeId=ct.courseTypeId",currPage,pageSize);
+    }
+
+    @Override
+    public CourseVo selCourse(Integer courseid) {
+        return (CourseVo) getObject(CourseVo.class,courseid);
+    }
+
+    @Override
+    public int selCourseCount() {
+        return selTotalRow("select count(*) from course");
+    }
+
+    @Override
+    public void updateCourse(CourseVo courseVo) {
+        updObject(courseVo);
+    }
+
+    @Override
+    public void delCourse(CourseVo courseVo) {
+        delObject(courseVo);
     }
 }
