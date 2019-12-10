0c6085b43cd6b57afeef61939964e7004a8db0a1
jx-Shihehua <2305654418@qq.com>
2019-12-10 20:57:25 +0800

    史何华 12/10 20:56

---
+++ src/com/ht/service/cemer/Impl/StudentHuorServiceImpl.java
@@ -0,0 +1,54 @@
+package com.ht.service.cemer.Impl;
+
+import com.ht.dao.BaseDao;
+import com.ht.service.cemer.IStudentHuorService;
+import com.ht.vo.student.StudentHuorVo;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created by 华 on 2019/12/10.
+ */
+@Service
+public class StudentHuorServiceImpl extends BaseDao implements IStudentHuorService {
+    @Override
+    public void addHuor(StudentHuorVo studentHuorVo) {
+        addObject(studentHuorVo);
+    }
+
+    @Override
+    public List selHuorList() {
+        return listByHql("from t_studentHuor");
+    }
+
+    @Override
+    public List selHuorPage(Integer currPage, Integer pageSize) {
+        return pageBySQL("select h.*,f.floorName from t_studentHuor h left join t_studentFloor f on h.floorId=f.floorId",currPage,pageSize);
+    }
+
+    @Override
+    public StudentHuorVo selHuor(Integer hourId) {
+        return (StudentHuorVo) getObject(StudentHuorVo.class,hourId);
+    }
+
+    @Override
+    public int selHuorCount() {
+        return selTotalRow("select count(*) from t_studentHuor");
+    }
+
+    @Override
+    public void delHuor(StudentHuorVo studentHuorVo) {
+        delObject(studentHuorVo);
+    }
+
+    @Override
+    public void delHuors(String ids) {
+        executeSQL("delete from t_studentHuor where hourId in("+ids+")");
+    }
+
+    @Override
+    public void updateHuor(StudentHuorVo studentHuorVo) {
+        updObject(studentHuorVo);
+    }
+}
